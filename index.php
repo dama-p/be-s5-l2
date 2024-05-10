23ff8c65ba753cdcc3e8ff4eac639967dfdb134d
Damaride Putignano <damarideputignano@gmail.com>
2024-05-10 17:25:23 +0200

    Added tables from db

--- index.php
+++ index.php
@@ -2,50 +2,41 @@
 include_once __DIR__ . '/db.php';
 require_once __DIR__ . '/includes/header.php';
 include_once __DIR__ . '/translations.php';
+
+
+if ($language === "it"){
+    $sqlQuery= "SELECT * FROM News_It";
+} else {
+    $sqlQuery = "SELECT * FROM News_En";
+}
+
+$stmt = $pdo->query($sqlQuery);
+$news = $stmt->fetchAll();
+
+
 ?>
 
 
     <div class="row g-2 mt-5">
+    <?php
+        foreach ($news as $row)
+                
+        { ?>
         <div class="col-sm-6 mb-3 mb-sm-0">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
+                    <h5 class="card-title"><?= $row['Title'] ?></h5>
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.
                     </p>
                     <a href="#" class="btn btn-primary"><?= $labels[$language]['details_btn'] ?></a>
                 </div>
             </div>
         </div>
-        <div class="col-sm-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.
-                    </p>
-                    <a href="#" class="btn btn-primary"><?= $labels[$language]['details_btn'] ?></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 mb-3 mb-sm-0">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.
-                    </p>
-                    <a href="#" class="btn btn-primary"><?= $labels[$language]['details_btn'] ?></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.
-                    </p>
-                    <a href="#" class="btn btn-primary"><?= $labels[$language]['details_btn'] ?></a>
-                </div>
-            </div>
-        </div>
+
+        <?php
+        } ?>
+
+        
     </div>
 
 <?php
